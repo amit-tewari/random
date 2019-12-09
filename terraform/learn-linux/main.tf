57c63b10ea7dd54d772b915cd5b2173ee73856aa
Amit Tewari <amit@datadewin.com>
2019-12-09 11:13:32 +0000

    upgrade config for tf 0.12

--- terraform/learn-linux/main.tf
+++ terraform/learn-linux/main.tf
@@ -11,73 +11,73 @@ provider "aws" {
 #variable "ami_key_pair_name" {}
 
 output "ami_name" {
- value = data.aws_ami.latest-debian.name
+  value = data.aws_ami.latest-debian.name
 }
 
 # Debian Account ID : 379101102735
 data "aws_ami" "latest-debian" {
   most_recent = true
-  owners = ["136693071363"] # Debian 10 SPI account
+  owners      = ["136693071363"] # Debian 10 SPI account
 
   filter {
-      name   = "name"
-      values = ["debian-10-amd64-*"]
+    name   = "name"
+    values = ["debian-10-amd64-*"]
   }
 
   filter {
-      name   = "virtualization-type"
-      values = ["hvm"]
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
 data "aws_ami" "latest-ubuntu" {
-most_recent = true
-owners = ["099720109477"] # Canonical
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
 
   filter {
-      name   = "name"
-      values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
   }
 
   filter {
-      name   = "virtualization-type"
-      values = ["hvm"]
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
 data "aws_ami" "centos" {
-owners      = ["679593333241"]
-most_recent = true
+  owners      = ["679593333241"]
+  most_recent = true
 
   filter {
-      name   = "name"
-      values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
   }
 
   filter {
-      name   = "architecture"
-      values = ["x86_64"]
+    name   = "architecture"
+    values = ["x86_64"]
   }
 
   filter {
-      name   = "root-device-type"
-      values = ["ebs"]
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 }
 
 data "aws_ami" "latest_ecs" {
-most_recent = true
-owners = ["591542846629"] # AWS
+  most_recent = true
+  owners      = ["591542846629"] # AWS
 
   filter {
-      name   = "name"
-      values = ["*amazon-ecs-optimized"]
+    name   = "name"
+    values = ["*amazon-ecs-optimized"]
   }
 
   filter {
-      name   = "virtualization-type"
-      values = ["hvm"]
-  }  
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 resource "aws_key_pair" "learn" {
@@ -86,65 +86,68 @@ resource "aws_key_pair" "learn" {
 }
 
 resource "aws_vpc" "learn" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 }
 
 resource "aws_subnet" "learn" {
-  cidr_block = "${cidrsubnet(aws_vpc.learn.cidr_block, 3, 1)}"
-  vpc_id = "${aws_vpc.learn.id}"
+  cidr_block        = cidrsubnet(aws_vpc.learn.cidr_block, 3, 1)
+  vpc_id            = aws_vpc.learn.id
   availability_zone = "us-east-1a"
 }
 
 resource "aws_security_group" "ingress-all-learn" {
-name = "allow-all-sg"
-vpc_id = "${aws_vpc.learn.id}"
-ingress {
+  name   = "allow-all-sg"
+  vpc_id = aws_vpc.learn.id
+  ingress {
     cidr_blocks = [
-      "0.0.0.0/0"
+      "0.0.0.0/0",
     ]
-from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
   }
-// Terraform removes the default rule
+
+  // Terraform removes the default rule
   egress {
-   from_port = 0
-   to_port = 0
-   protocol = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "learn" {
-  ami = "${data.aws_ami.latest-debian.id}"
-  instance_type = "t2.micro"
-  key_name = "${aws_key_pair.learn.key_name}"
-  security_groups = ["${aws_security_group.ingress-all-learn.id}"]
-  subnet_id = "${aws_subnet.learn.id}"
+  ami             = data.aws_ami.latest-debian.id
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.learn.key_name
+  security_groups = [aws_security_group.ingress-all-learn.id]
+  subnet_id       = aws_subnet.learn.id
   tags = {
     name = data.aws_ami.latest-debian.name
   }
 }
 
 resource "aws_eip" "learn" {
-  instance = "${aws_instance.learn.id}"
+  instance = aws_instance.learn.id
   vpc      = true
 }
 
 resource "aws_internet_gateway" "learn" {
-  vpc_id = "${aws_vpc.learn.id}"
+  vpc_id = aws_vpc.learn.id
 }
 
 resource "aws_route_table" "learn" {
-  vpc_id = "${aws_vpc.learn.id}"
-route {
+  vpc_id = aws_vpc.learn.id
+  route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.learn.id}"
+    gateway_id = aws_internet_gateway.learn.id
   }
 }
+
 resource "aws_route_table_association" "learn" {
-  subnet_id      = "${aws_subnet.learn.id}"
-  route_table_id = "${aws_route_table.learn.id}"
+  subnet_id      = aws_subnet.learn.id
+  route_table_id = aws_route_table.learn.id
 }
+
